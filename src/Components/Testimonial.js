303f874ffb6cdf6cf7409864b2921d8877e6020a
Pao <paulinemarie.didier@gmail.com>
2021-06-29 17:46:16 +0200

    create testimonial component

---
+++ src/Components/Testimonial.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import emily from '../images/image-emily.jpg'
+
+
+export default function Testimonial({avatar, text, name, job}) {
+    return (
+        <article className="flex flex-col p-10 space-y-6">
+            <div>
+                <img className="rounded-full w-20 mx-auto" src={avatar} alt=""/>
+            </div>
+            <p className="text-dark-grayish-blue font-bold font-barlow">{text}</p>
+            <div>
+                <span className="font-fraunces">{name}</span>
+                <br/>
+                <span className="font-barlow text-grayish-blue">{job}</span>
+            </div>
+        </article>
+    )
+}
